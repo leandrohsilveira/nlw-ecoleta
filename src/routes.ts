5987a52ff6705b94fe04adb777683a046afe726d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-05 14:21:39 -0300

    Move static router to root router module

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import path from "path";
 import itemRouter from "./domain/item/routes";
 import pointRouter from "./domain/point/routes";
 
 const routes = express.Router();
 
+routes.use(
+  "/uploads",
+  express.static(path.resolve(__dirname, "..", "uploads"))
+);
 routes.use("/items", itemRouter);
 routes.use("/points", pointRouter);
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import path from "path";
 import routes from "./routes";
 import config from "./config";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
-
-app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(config.getPort(), () => console.log("Server started"));

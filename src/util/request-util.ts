8c62f9b464632b6940ecbf0447453d2baeff95ed
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-05 12:02:53 -0300

    Add queryAsArray util function

--- src/util/request-util.ts
+++ src/util/request-util.ts
@@ -1,9 +1,30 @@
 import { Request } from "express";
 import config from "../config";
 
+interface QueryStringParamParser<T> {
+  (value: string): T;
+}
+
+function getServerBaseUrl(request: Request): string {
+  return `${request.protocol}://${request.hostname}:${config.getPort()}`;
+}
+
+function queryAsArray<T>(
+  request: Request,
+  paramName: string,
+  parser: QueryStringParamParser<T>
+): T[] {
+  const value = request.query[paramName];
+  if (value) {
+    if (Array.isArray(value)) return (value as string[]).map(parser);
+    else return [parser(value as string)];
+  }
+  return [];
+}
+
 const requestUtil = {
-  getServerBaseUrl: (request: Request): string =>
-    `${request.protocol}://${request.hostname}:${config.getPort()}`,
+  getServerBaseUrl,
+  queryAsArray,
 };
 
 export default requestUtil;

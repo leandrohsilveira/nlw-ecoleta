3476aee877d79ab49cf297327fc2407fb02873fb
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-05 12:02:37 -0300

    Fix count util function

--- src/util/query-util.ts
+++ src/util/query-util.ts
@@ -1,8 +1,17 @@
 import Knex from "knex";
 
-async function count(query: Knex.QueryBuilder): Promise<number> {
-  const [{ "count(*)": result }] = await query.count();
-  return result;
+async function count(
+  query: Knex.QueryBuilder,
+  countItem = "*",
+  distinct = false
+): Promise<number> {
+  let result: { [key: string]: number };
+  if (distinct) {
+    result = await query.countDistinct(countItem).first();
+  } else {
+    result = await query.count(countItem).first();
+  }
+  return Object.keys(result).map((key) => result[key])[0];
 }
 
 const queryUtil = {

bfe9db575623b6accfed28e36766f49845b3cf16
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-05 14:22:01 -0300

    Apply cors module to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes";
 import config from "./config";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors(config.getCors()));
 app.use(express.json());
 app.use(routes);
 

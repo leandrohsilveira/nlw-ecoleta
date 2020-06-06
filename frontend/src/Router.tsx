6b8f940c6ea666b94cfe8cdca0a8917f3adbb2c6
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-05 21:29:20 -0300

    Make lazy loading on Router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,13 +1,17 @@
 import React, { Suspense } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+
+const Home = React.lazy(() => import("./domain/Home"));
+const CreatePoint = React.lazy(() => import("./domain/point/CreatePoint"));
+
 const Loading = () => <div>Loading...</div>;
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
-      <Route component={Home} path="/" exact />
-      <Route component={CreatePoint} path="/create-point" exact />
+        <Route component={Home} path="/" exact />
+        <Route component={CreatePoint} path="/create-point" exact />
       </Suspense>
     </BrowserRouter>
   );

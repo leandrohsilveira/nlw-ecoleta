b9b061be335697f4fd132f132e8a20b52d8f048d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-07 13:00:07 -0300

    Create Point model

---
+++ frontend/src/domain/point/model/index.ts
@@ -0,0 +1,11 @@
+export interface Point {
+  name: string;
+  image: string;
+  email: string;
+  whatsapp: string;
+  latitude: number;
+  longitude: number;
+  uf: string;
+  city: string;
+  items: number[];
+}

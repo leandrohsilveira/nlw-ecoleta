ae61b549e287f84b4e1106c9f21f48114244367b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-08 00:25:34 -0300

    Import fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
+import { AppLoading } from "expo";
+import Home from "./components/Home";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  });
+
+  if (fontsLoaded)
+    return (
+      <>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <Home />
+      </>
+    );
+  else return <AppLoading />;
 }
 
 const styles = StyleSheet.create({

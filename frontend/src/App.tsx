5c3896d752d8c842f6eeceeea5be4359ca949408
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-10 16:01:58 -0300

    Frontend: utilizar interface EcoletaCoreConfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { applyConfigurations } from "ecoleta-core";
 import "./App.css";
 import Router from "./components/Router";
+import config from "./config";
+
+applyConfigurations(config);
 
 function App() {
   return (
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,11 @@
+import { EcoletaCoreConfig } from "ecoleta-core";
+
 function getBackendBaseUrl() {
   return process.env.BACKEND_BASE_URL ?? "http://localhost:3333";
 }
 
-const config = {
-  getBackendBaseUrl,
+const config: EcoletaCoreConfig = {
+  backendBaseUrl: getBackendBaseUrl(),
 };
 
 export default config;

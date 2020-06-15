ab6c73affe72df528719bd61568fd7b2b840ef2d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2020-06-15 12:55:37 -0300

    Backend: use custom address geolocation format

--- backend/src/domain/geolocation/model.ts
+++ backend/src/domain/geolocation/model.ts
@@ -13,6 +13,10 @@ interface OpenCageDataComponent {
   city?: string;
   town?: string;
   village?: string;
+  suburb?: string;
+  road?: string;
+  house_number?: string;
+  postcode?: string;
   continent: string;
   country: string;
   country_code: string;
@@ -32,9 +36,17 @@ export interface OpenCageDataResponse {
   results: OpenCageDataResultItem[];
 }
 
-export interface GeolocationJson
-  extends OpenCageDataComponent,
-    OpenCageDataGeometry {
+export interface GeolocationJson extends OpenCageDataGeometry {
+  city?: string;
+  suburb?: string;
+  road?: string;
+  house_number?: string;
+  postcode?: string;
+  continent: string;
+  country: string;
+  country_code: string;
+  state: string;
+  state_code: string;
   latDelta: number;
   lngDelta: number;
   formatted: string;
@@ -53,18 +65,46 @@ export function serializeResponse(
     ) ?? response.results[0];
   if (item) {
     const {
-      components,
+      components: {
+        _type,
+        town,
+        village,
+        city,
+        continent,
+        country,
+        country_code,
+        state,
+        state_code,
+        suburb,
+        road,
+        house_number,
+        postcode,
+      },
       geometry: { lat, lng },
       bounds: { northeast, southwest },
-      formatted,
     } = item;
-
+    const _city = city ?? town ?? village;
     return {
-      ...components,
-      city: components.city ?? components.town ?? components.village,
+      city: _city,
+      continent,
+      country,
+      country_code,
+      state,
+      state_code,
       lat,
       lng,
-      formatted,
+      suburb,
+      road,
+      house_number,
+      postcode,
+      formatted: [
+        `${road ?? ""} ${house_number ?? ""}`.trim(),
+        suburb,
+        `${_city} - ${state_code}`,
+        postcode,
+      ]
+        .filter((i) => !!i)
+        .join(", "),
       latDelta: Math.abs(northeast.lat - southwest.lat),
       lngDelta: Math.abs(northeast.lng - southwest.lng),
     };
